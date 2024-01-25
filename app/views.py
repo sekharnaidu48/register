fb59bea5f32654ddcdc33bafb58574e5a42ddbea
sekharnaidu23 <sekharnaidu23@gmail.com>
2024-01-25 17:51:54 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,35 +1,82 @@
 from django.shortcuts import render
-from app.forms import *
-from django.http import HttpResponse
-from django.core.mail import send_mail
+
 # Create your views here.
+
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
+
     if request.method=='POST' and request.FILES:
         ufd=UserForm(request.POST)
         pfd=ProfileForm(request.POST,request.FILES)
+
         if ufd.is_valid() and pfd.is_valid():
             MUFDO=ufd.save(commit=False)
             pw=ufd.cleaned_data['password']
             MUFDO.set_password(pw)
             MUFDO.save()
+
             MPFDO=pfd.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
-            send_mail('regisstration',
-                      'Your registration is successfully',
-                      'sekharnaidu23@gmail.com',
-                      [MUFDO.email],
-                      fail_silently=True
+            send_mail('registration',
+            ' Thank U ur Registration is scuccessfull',
+            'harshadvali91@gmail.com',
+            [MUFDO.email],
+            fail_silently=False)
+            
 
-            )
+            return HttpResponse('Registartion Is Successfull')
+        else:
+            return HttpResponse('Invalid Data')
 
-
-
-
-
-
-            return HttpResponse('Registration successfully')
     return render(request,'registration.html',d)
+
+
+
+def Home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'Home.html',d)
+    return render(request,'Home.html')
+
+
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('Home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Home'))
+
+@login_required
+def profile_display(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request ,'profile_display.html',d)
--- register/urls.py
+++ register/urls.py
@@ -22,4 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
+    path('Home/',Home,name='Home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
+    path('profile_display/',profile_display,name='profile_display'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
